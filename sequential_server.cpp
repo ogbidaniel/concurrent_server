52d8fbcd630a6ba7cadb057d037f358ab8598c2c
Daniel Ogbuigwe <103327095+ogbidaniel@users.noreply.github.com>
2024-12-25 23:58:01 -0600

    windows port

--- sequential_server.cpp
+++ sequential_server.cpp
@@ -1,91 +1,98 @@
-// Sequential socket server - accepting one client at a time.
-//
-// Eli Bendersky [http://eli.thegreenplace.net]
-// This code is in the public domain.
-#include <stdint.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
 
-#include "utils.h"
+#pragma comment(lib, "ws2_32.lib")
 
 typedef enum { WAIT_FOR_MSG, IN_MSG } ProcessingState;
 
-void serve_connection(int sockfd) {
-  // Clients attempting to connect and send data will succeed even before the
-  // connection is accept()-ed by the server. Therefore, to better simulate
-  // blocking of other clients while one is being served, do this "ack" from the
-  // server which the client expects to see before proceeding.
-  if (send(sockfd, "*", 1, 0) < 1) {
-    perror_die("send");
-  }
-
-  ProcessingState state = WAIT_FOR_MSG;
-
-  while (1) {
-    uint8_t buf[1024];
-    int len = recv(sockfd, buf, sizeof buf, 0);
-    if (len < 0) {
-      perror_die("recv");
-    } else if (len == 0) {
-      break;
-    }
-
-    for (int i = 0; i < len; ++i) {
-      switch (state) {
-      case WAIT_FOR_MSG:
-        if (buf[i] == '^') {
-          state = IN_MSG;
-        }
-        break;
-      case IN_MSG:
-        if (buf[i] == '$') {
-          state = WAIT_FOR_MSG;
-        } else {
-          buf[i] += 1;
-          if (send(sockfd, &buf[i], 1, 0) < 1) {
-            perror("send error");
-            close(sockfd);
-            return;
-          }
-        }
-        break;
-      }
-    }
-  }
-
-  close(sockfd);
+void perror_die(const char* message) {
+    fprintf(stderr, "%s: %d\n", message, WSAGetLastError());
+    exit(1);
 }
 
-int main(int argc, char** argv) {
-  setvbuf(stdout, NULL, _IONBF, 0);
-
-  int portnum = 9090;
-  if (argc >= 2) {
-    portnum = atoi(argv[1]);
-  }
-  printf("Serving on port %d\n", portnum);
-
-  int sockfd = listen_inet_socket(portnum);
-
-  while (1) {
-    struct sockaddr_in peer_addr;
-    socklen_t peer_addr_len = sizeof(peer_addr);
-
-    int newsockfd =
-        accept(sockfd, (struct sockaddr*)&peer_addr, &peer_addr_len);
-
-    if (newsockfd < 0) {
-      perror_die("ERROR on accept");
+void serve_connection(SOCKET sockfd) {
+    if (send(sockfd, "*", 1, 0) < 1) {
+        perror_die("send");
     }
 
-    report_peer_connected(&peer_addr, peer_addr_len);
-    serve_connection(newsockfd);
-    printf("peer done\n");
-  }
+    ProcessingState state = WAIT_FOR_MSG;
 
-  return 0;
+    while (1) {
+        uint8_t buf[1024];
+        int len = recv(sockfd, (char*)buf, sizeof buf, 0);
+        if (len < 0) {
+            perror_die("recv");
+        } else if (len == 0) {
+            break;
+        }
+
+        for (int i = 0; i < len; ++i) {
+            switch (state) {
+            case WAIT_FOR_MSG:
+                if (buf[i] == '^') {
+                    state = IN_MSG;
+                }
+                break;
+            case IN_MSG:
+                if (buf[i] == '$') {
+                    state = WAIT_FOR_MSG;
+                } else {
+                    buf[i] += 1;
+                    if (send(sockfd, (char*)&buf[i], 1, 0) < 1) {
+                        perror_die("send");
+                    }
+                }
+                break;
+            }
+        }
+    }
+
+    closesocket(sockfd);
+}
+
+int main() {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        perror_die("WSAStartup failed");
+    }
+
+    SOCKET sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == INVALID_SOCKET) {
+        perror_die("socket");
+    }
+
+    struct sockaddr_in server_addr = { 0 };
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(9090);
+
+    if (bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
+        perror_die("bind");
+    }
+
+    if (listen(sockfd, SOMAXCONN) == SOCKET_ERROR) {
+        perror_die("listen");
+    }
+
+    printf("Server listening on port 9090...\n");
+
+    while (1) {
+        struct sockaddr_in client_addr;
+        int addr_len = sizeof(client_addr);
+        SOCKET client_sock = accept(sockfd, (struct sockaddr*)&client_addr, &addr_len);
+        if (client_sock == INVALID_SOCKET) {
+            perror_die("accept");
+        }
+
+        printf("Client connected\n");
+        serve_connection(client_sock);
+        printf("Client disconnected\n");
+    }
+
+    closesocket(sockfd);
+    WSACleanup();
+    return 0;
 }
